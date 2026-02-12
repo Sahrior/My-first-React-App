433683d2b0acf89c5b22c2bb6b0b5160d24c01fa
Md Sahrior Haider Shovon <165468630+Sahrior@users.noreply.github.com>
2026-02-13 00:08:44 +0600

    topics added

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,11 +1,73 @@
 import React from 'react'
 
 const App = () => {
+
+
+  let marks = 75;
+  const city = ['dhaka','london','delhi','kolkata']
+
   return (
     <>
-      <h1>Hello Afifa</h1>
-      <h3>Hello Sahrior</h3>
-      <h1>{2+2}</h1>
+
+
+      {/* JSX basic */}
+
+
+      <h5 style={
+        {color:'red'}
+      } >Hello Afifa</h5>
+      <h5>Hello Sahrior</h5>
+      <h5>{2+2}</h5>
+
+      <div>
+        <button onClick={()=>alert('Hello')} >Submiit</button>
+      </div>
+
+
+      {/* if else */}
+
+      <div>
+        {
+          marks>80?
+          <h5>Good</h5>:
+          <h5>Average</h5>
+        }
+      </div>
+
+      {/* immediately invoked function */}
+
+      <div>
+
+        {(()=>{
+
+          if(marks>80 && marks<100){
+            return "Got between 80 - 100"
+          }else{
+            return "below 80"
+          }
+
+          })()}
+
+
+      </div>
+
+
+      {/* Loops */}
+
+      <div>
+
+          <ul>
+            {
+              city.map((item,i)=>{
+                return  ( <li key={i.toString()} >{item}</li> )
+              })
+            }
+          </ul>
+
+      </div>
+
+
+
       
     </>
   )
